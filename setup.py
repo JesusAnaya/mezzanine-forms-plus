8378caa106ca2fd6eba465e326ffd45c88df5df8
Jesus Anaya <jesus.anaya.dev@gmail.com>
2014-09-12 15:53:22 -0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+    version='0.0.1',
+    license="BSD",
+    description='Mezzanine Form page alternative',
+    author='Jesus Anaya',
+    author_email='jesus.anaya.dev@gmail.com',
+    url='https://github.com/JesusAnaya/mezzanine-forms-plus/',
+    packages=['forms_plus'],
+)
